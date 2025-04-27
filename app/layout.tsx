d2d828a0c53112dbe1b5c36b32c8629653b64e92
Daniel González Briseño <qbixmex@gmail.com>
2025-04-27 12:02:03 -0600

    Adding Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
+import { FC } from 'react';
+import { inter } from './ui/fonts';
 import '@/app/ui/global.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+type Props = Readonly<{ children: React.ReactNode }>;
+
+const RootLayout: FC<Props> = ({ children }) => {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
