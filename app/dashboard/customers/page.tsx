42368fe7b50be3cb6e733d70a4b46ee2b8aec7eb
Daniel González Briseño <qbixmex@gmail.com>
2025-04-27 12:25:40 -0600

    Working with Nested Layouts and Pages

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+export const CustomersPage = () => {
+  return (
+    <h1 className="text-3xl text-blue-800 font-semibold">
+      Customers Page
+    </h1>
+  );
+};
+
+export default CustomersPage;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export const DashboardPage = () => {
+  return (
+    <h1 className="text-3xl text-blue-800 font-semibold">
+      Dashboard Page
+    </h1>
+  );
+};
+
+export default DashboardPage;
